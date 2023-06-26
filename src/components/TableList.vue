c8eb87e08c72ea6bdc0544c51ddc19aeaf843893
Murilo-MRS <murilosantana1993@gmail.com>
2023-06-25 22:45:35 -0300

    feat: add funcionalidade de remover usuarios

--- src/components/TableList.vue
+++ src/components/TableList.vue
@@ -24,37 +24,54 @@
           <th class="text-start whitespace-nowrap px-4 py-2 font-medium text-gray-900">
             Criado por
           </th>
-          <th class="px-4 py-2"></th>
         </tr>
       </thead>
 
-      <tbody class="divide-y divide-gray-200">
-        <tr v-for="employee in team" :key="employee.id">
-          <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-            {{ employee.id }}
-          </td>
-          <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.nome }}</td>
-          <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.sobrenome }}</td>
-          <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.cargo }}</td>
-          <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.dataInicio }}</td>
-          <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.ativo ? 'Sim' : 'Não'}}</td>
-          <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.criador }}</td>
-          <td class="whitespace-nowrap px-4 py-2">
-            <button
-              class="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-            >
-              Remover
-            </button>
-          </td>
-          <td class="whitespace-nowrap px-4 py-2">
-            <button
-              class="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-            >
-              Editar
-            </button>
-          </td>
-        </tr>
-      </tbody>
+      <template v-if="team.length > 0">
+        <tbody class="divide-y divide-gray-200">
+          <tr v-for="employee in team" :key="employee.id">
+            <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+              {{ employee.id }}
+            </td>
+            <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.nome }}</td>
+            <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.sobrenome }}</td>
+            <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.cargo }}</td>
+            <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ new Date(employee.dataInicio as string).toLocaleDateString('pt-BR') }}</td>
+            <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.ativo ? 'Ativo' : 'Desativado'}}</td>
+            <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ employee.criador.nome }}</td>
+            <td class="flex whitespace-nowrap px-4 py-2 gap-1">
+              <button
+                class="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-red-700"
+                v-if="!toDelete"
+                @click="handleRemove"
+              >
+              <TrashIcon class="h-fit w-6 text-white"/>
+              </button>
+              <div v-if="toDelete" class="flex gap-1">
+                <button
+                  class="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-red-700"
+                  @click="handleDeleteConfirmation(employee.id as number)"
+                >
+                  <CheckIcon class="h-fit w-6 text-white" />
+                </button>
+                <button
+                  class="inline-block rounded bg-gray-100 text-red-600 px-4 py-2 text-xs font-medium hover:bg-gray-200"
+                  @click="handleRemove"
+                >
+                  <XCircleIcon class="h-fit w-6" />
+                </button>
+              </div>
+            </td>
+            <td class="whitespace-nowrap px-4 py-2">
+              <button
+                class="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+                >
+                <PencilSquareIcon class="h-fit w-6 text-red" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </template>
     </table>
   </div>
 
@@ -63,11 +80,26 @@
 <script setup lang="ts">
 import type { IEmployee } from '@/interfaces/IEmployee';
 import type { PropType } from 'vue';
+import { TrashIcon, XCircleIcon, CheckIcon, PencilSquareIcon } from '@heroicons/vue/24/outline';
+
+const emit = defineEmits(['delete', 'edit', 'confirm-delete']);
+
+const handleDeleteConfirmation = (id: number) => {
+  emit('confirm-delete', id);
+};
+
+const handleRemove = () => {
+  emit('delete');
+};
 
 defineProps({
   team: {
     type: Array as PropType<IEmployee[]>,
     required: true,
   },
+  toDelete: {
+    type: Boolean as PropType<boolean>,
+    required: false,
+  }
 });
 </script>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,9 +1,144 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex flex-col items-center">
+    <div class="items-center rounded-md border-2 border-gray-300 p-4">
+      <form @submit.prevent="() => handleCreateUser(newEmployee)" class="flex flex-col items-center">
+        <span class="mb-4 font-bold text-lg">
+          Cadastrar usuário
+        </span>
+        <div class="flex w-full">
+          <div class="px-2 firstname">
+            <input
+              type="text"
+              placeholder="Nome"
+              v-model="newEmployee.nome"
+              class="w-full rounded-md h-[30px] pl-3 bg-gray-100 focus:outline-indigo-600"
+            />
+          </div>
+          <div class="px-2 secondname">
+            <input
+            type="text"
+            placeholder="Sobrenome"
+            v-model="newEmployee.sobrenome"
+            class="w-full rounded-md h-[30px] pl-3 bg-gray-100 focus:outline-indigo-600"
+            />
+          </div>
+        </div>
+        <div class="flex w-full my-2 gap-2">
+          <div class="px-2 container">
+            <select
+              v-model="newEmployee.cargo"
+              class="w-full rounded-md h-[30px] pl-3 bg-gray-100 focus:outline-indigo-600"
+            >
+              <option value="DESENVOLVEDOR" selected>DESENVOLVEDOR</option>
+            </select>
+          </div>
+          <div class="px-2 container">
+            <input
+              type="datetime-local"
+              placeholder="Data de Início"
+              v-model="newEmployee.dataInicio"
+              class="w-full rounded-md h-[30px] pl-3 bg-gray-100 focus:outline-indigo-600"
+            />
+          </div>
+          <div class="px-2 w-full">
+            <button
+              type="submit"
+              class="container rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700 disabled:bg-indigo-400"
+              :disabled="isDisabled"
+            >
+              Criar
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <TableList
+      :toDelete="toDelete"
+      :team="team"
+      @confirm-delete='(id) => handleDeleteConfirmation(id)'
+      @delete='() => handleDeleteUser()'
+    />
   </main>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted, computed } from 'vue';
+import api, { creatorId, creatorName } from '@/services/api';
+import type { IEmployee } from '@/interfaces/IEmployee';
+import TableList from '@/components/TableList.vue';
+
+const team = ref<IEmployee[]>([]);
+
+const toDelete = ref<boolean>(false);
+
+const newEmployee = ref<IEmployee>({
+  nome: '',
+  sobrenome: '',
+  cargo: 'DESENVOLVEDOR',
+  dataInicio: '',
+  ativo: true,
+  criador: {
+    id: creatorId,
+    nome: creatorName
+  },
+});
+
+const isDisabled = computed(() => {
+  return newEmployee.value.nome && newEmployee.value.sobrenome && newEmployee.value.dataInicio ? false : true;
+});
+
+const showModal = ref(false);
+
+const fetchTeam = async () => {
+  const { data } = await api.get(`/funcionario?criadorId=${creatorId}`);
+  return data;
+};
+
+const handleShowModal = () => {
+  showModal.value = !showModal.value;
+};
+
+const handleCreateUser = async (addEmployee: IEmployee) => {
+  try {
+    newEmployee.value = addEmployee;
+    console.log(newEmployee.value.dataInicio);
+    
+
+    await api.post(`/funcionario?criadorId=${creatorId}`, newEmployee.value);
+
+    newEmployee.value = {
+      nome: '',
+      sobrenome: '',
+      cargo: 'DESENVOLVEDOR',
+      dataInicio: '',
+      ativo: true,
+      criador: {
+        id: creatorId,
+        nome: creatorName
+      },
+    };
+
+    team.value = await fetchTeam();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const handleDeleteUser = async () => {
+  toDelete.value = true;
+};
+
+const handleDeleteConfirmation = async (id: string) => {
+  try {
+    await api.delete(`/funcionario/${id}?criadorId=${creatorId}`);
+    team.value = await fetchTeam();
+  } catch (error) {
+    console.error(error);
+  }
+};
+  
+onMounted(async () => {
+  team.value = await fetchTeam();
+});
+
+</script>
