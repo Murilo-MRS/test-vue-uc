220dffacd7effc318ab9f17ad5cbe9f7fcda5674
Murilo-MRS <murilosantana1993@gmail.com>
2023-06-25 22:44:45 -0300

    fix: colocando tipagem correta

--- src/interfaces/IEmployee.ts
+++ src/interfaces/IEmployee.ts
@@ -1,11 +1,11 @@
 import type { ICreator } from './ICreator';
 
 export interface IEmployee {
-  id: number,
-  nome: 'string',
-  sobrenome: 'string',
-  cargo: 'string',
-  dataInicio: 'string',
+  id?: number,
+  nome?: string,
+  sobrenome?: string,
+  cargo: string,
+  dataInicio?: string,
   ativo: boolean,
   criador: ICreator
 }
